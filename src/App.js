22cb84f7a152169c710007d94d50d018c08a074a
gizem28 <gizemodun28@gmail.com>
2021-12-27 00:04:26 +0100

    m

---
+++ src/App.js
@@ -0,0 +1,170 @@
+import './App.css';
+import Header from './components/Header';
+import {
+  useState, useEffect
+} from 'react';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+import axios from 'axios';
+// import image from "./sky.jpg";
+
+function App() {
+
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const baseUrl ="https://task-tracker-backend-gzm.herokuapp.com/";
+  // const baseUrl = "http://localhost:5000/tasks";
+
+  const [tasks, setTasks] = useState([
+  //   {
+  //     id: 1,
+  //     text: "Study React Pre-Class Notes",
+  //     day: "Dec 12th at 2:30pm",
+  //     isDone: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: "Feed the Dog",
+  //     day: "Dec 13th at 1:30pm",
+  //     isDone: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: "Attend In-Class",
+  //     day: "Dec 14th at 3:00pm",
+  //     isDone: false,
+  //   },
+  ]);
+
+
+//Create Read Update Delete
+
+//Fetch tasks
+// const fetchTask= async()=>{
+//   const res = await fetch(baseUrl)
+//   const data =await res.json();
+//   console.log(data)};
+
+//Fetck Task with axios
+const fetchTask=async()=>{
+  // const res= await axios.get(baseUrl);
+  const {data}= await axios.get(baseUrl);
+  setTasks(data);
+};
+
+useEffect(()=>{
+  fetchTask()
+  }, [])
+
+
+  //*Add Task Fetch Function
+  // const addTask = async(newTask)=>{
+  //   const res = await fetch(baseUrl,{
+  //     method:"POST",
+  //     headers:{
+  //       "Content-type":"application/json"
+  //     },
+  //     body:JSON.stringify(newTask)
+  //   });
+  //   fetchTask(res);
+  // };
+
+  //*Add Task Axios
+  const addTask = async(newTask)=>{
+    await axios.post(baseUrl, newTask)
+    fetchTask();
+  }
+
+  // const addTask = (newTask) => {
+  //   const id = Math.floor(Math.random() * 100) + 1;
+  //   const addNewTask = {
+  //     id,
+  //     ...newTask
+  //   };
+  //   setTasks([...tasks, addNewTask])
+  // }
+
+  //*Delete Task
+  // const deleteTask = (deletedTaskId) => {
+  //   console.log("delete", deletedTaskId);
+  //   setTasks(tasks.filter((task) => task.id !== deletedTaskId))
+  // }
+
+  //*Delete with Fetch
+  // const deleteTask= async (deletedTaskId)=>{
+  // await fetch(`${baseUrl}/${deletedTaskId}`, {
+  //   method:"DELETE",
+  // });
+  // fetchTask();
+  // };
+
+  //*Delete with Axios
+  const deleteTask = async(deletedTaskId)=>{
+    await axios.delete(`${baseUrl}/${deletedTaskId}`);
+    fetchTask();
+  };
+
+  //*Toggle Done
+  // const toggleDone = (toggleDoneId) => {
+  //   setTasks(
+  //     tasks.map((task) => task.id === toggleDoneId ? {
+  //       ...task,
+  //       isDone: !task.isDone
+  //     } : task)
+  //   )
+  // }
+
+  //*Toggle Done Fetch
+    const toggleDone = async(toggleDoneId)=>{
+    // const res= await fetch(`${baseUrl}/${toggleDoneId}`)
+    // const data =await res.json();
+    // const updatedTask={...data, isDone: !data.isDone};
+  //   await fetch(`${baseUrl}/${toggleDoneId}`, {
+  //     method:"PUT",
+  //     headers:{
+  //       "Content-type":"application/json",
+  //     },
+  //     body: JSON.stringify(updatedTask),
+  //   });
+  //   fetchTask();
+  // };
+
+   //*Toggle Done Axios
+   const {data}=await axios.get(`${baseUrl}/${toggleDoneId}`);
+   const updatedTask={...data, isDone:!data.isDone};
+
+   await axios.put(`${baseUrl}/${toggleDoneId}`, updatedTask);
+   fetchTask();
+  };
+   
+
+  //SHOW ADD TASK
+  const toggleShow = () => setShowAddTask(!showAddTask);
+
+  return ( <div className = "container" >
+    <Header title = "Task Tracker"
+    showAddTask = {showAddTask}
+    toggleShow = {toggleShow}/>
+
+    {
+      showAddTask && < AddTask addTask = {addTask}/>}
+
+      {
+        tasks.length > 0 ? ( < Tasks tasks= {tasks}
+          deleteTask = {
+            deleteTask
+          }
+          toggleDone = {
+            toggleDone
+          }
+          />
+          ):( 
+            <p style = {{
+              textAlign: "center",
+              color: "gray"
+            }} > No Task To Show </p>
+        )} </div>
+    );
+  }
+
+export default App;
