de1758b471bc6197ed144c8c64a1bf21fbf7a6d0
gizem28 <gizemodun28@gmail.com>
2021-12-31 22:30:09 +0100

    m

--- src/App.js
+++ src/App.js
@@ -12,29 +12,10 @@ function App() {
 
   const [showAddTask, setShowAddTask] = useState(false)
 
-  const baseUrl ="https://task-tracker-backend-gzm.herokuapp.com/";
+  const baseUrl ="https://task-tracker-backend-gzm.herokuapp.com/tasks";
   // const baseUrl = "http://localhost:5000/tasks";
 
-  const [tasks, setTasks] = useState([
-  //   {
-  //     id: 1,
-  //     text: "Study React Pre-Class Notes",
-  //     day: "Dec 12th at 2:30pm",
-  //     isDone: false,
-  //   },
-  //   {
-  //     id: 2,
-  //     text: "Feed the Dog",
-  //     day: "Dec 13th at 1:30pm",
-  //     isDone: true,
-  //   },
-  //   {
-  //     id: 3,
-  //     text: "Attend In-Class",
-  //     day: "Dec 14th at 3:00pm",
-  //     isDone: false,
-  //   },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
 
 //Create Read Update Delete
